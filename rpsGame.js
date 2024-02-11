875c037cf1cf78def4fde604403e663fc6cd8145
Diego Alexandre <diegoalexandre_@life.com>
2024-02-11 14:53:16 -0300

    Initial basics for the project
    Create all necessary files for the game.

---
+++ rpsGame.js
@@ -0,0 +1,23 @@
+// Create the object with the game itens
+
+// Create a round time number
+
+// Create score count for the computer and for the player
+
+// Game starts one round
+
+// The computer roll the dice to choose one item
+
+// The player choose one item
+
+// Game checks the round winner
+
+// Winner receive 1 point to its score
+
+// Loop the round X times
+
+// Game checks the score point winner 
+
+// Show game winner message
+
+// Asks to play again
